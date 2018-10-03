d06e46b3bc0dc627c78ebd84d67ee54e12e9b3cd
Geoff Phillips <geoffreymartinphillips@gmail.com>
2018-10-03 11:19:38 -0700

    Create index function for board_items controller

--- app/controllers/board_items_controller.rb
+++ app/controllers/board_items_controller.rb
@@ -3,12 +3,23 @@ class BoardItemsController < ApplicationController
     @user = current_user
     post = Post.find(board_items_params[:id])
     @boardItems = post.board_items.count
+    @favorites_board = @user.boards.find_by(name: "Favorites")
 
-    render json: { board_items: @boardItems, boards: @boards }
+    favorite = is_favorite?
+
+    render json: { board_items: @boardItems, favorite: favorite }
   end
 
   private
     def board_items_params
       params.require(:board_items).permit!
+    end
+
+    def is_favorite?
+      if @favorites_board
+        return true
+      else
+        return false
+      end
     end
 end
